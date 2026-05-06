f28efb1758ccda0cfd7511e052ee05112be37553
Andres Mosquera <jufepepa@gmail.com>
2026-05-06 18:02:54 -0500

    Variables de entorno

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9SZOdtse4S3mwtC_SwUUdoYKpuGsAeQA",
-  authDomain: "shinny-woman-store.firebaseapp.com",
-  projectId: "shinny-woman-store",
-  storageBucket: "shinny-woman-store.firebasestorage.app",
-  messagingSenderId: "30589985607",
-  appId: "1:30589985607:web:d729a7ae12bb17bf686262"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-export const db = getFirestore(app)
